132a3303421c32f1786473af8f21543ff1d2de12
Daniel Dara <ddara@g.clemson.edu>
2016-12-06 01:54:22 -0500

    Cleaned up imports and removed some logging.

--- 2016/day5/part1.py
+++ 2016/day5/part1.py
@@ -1,16 +1,14 @@
-import hashlib
-import unicodedata
+from hashlib import md5
 
 password = ''
 i = 0
 
 while len(password) < 8:
-	hash = hashlib.md5(bytes('abbhdwsy' + str(i), 'ascii')).hexdigest()
+	hash = md5(bytes('abbhdwsy' + str(i), 'ascii')).hexdigest()
 
 	if hash.startswith('00000'):
-		print("Found character '" + hash[5] + "' " + str(7 - len(password)) + " more to go.")
 		password += hash[5]
 
 	i += 1
 
-print("Completed password: " + password)
+print(password)
--- 2016/day5/part2.py
+++ 2016/day5/part2.py
@@ -1,14 +1,12 @@
-import hashlib
-import unicodedata
+from hashlib import md5
 
 password = {}
 i = 0
 
 while len(password.values()) < 8:
-	hash = hashlib.md5(bytes('abbhdwsy' + str(i), 'ascii')).hexdigest()
+	hash = md5(bytes('abbhdwsy' + str(i), 'ascii')).hexdigest()
 
 	if hash.startswith('00000') and hash[5].isdigit() and int(hash[5]) <= 7 and int(hash[5]) not in password:
-		print("Found character '" + hash[6] + "' for position " + hash[5] + ", " + str(8 - len(password.values())) + " more to go.")
 		password[int(hash[5])] = hash[6]
 
 	i += 1

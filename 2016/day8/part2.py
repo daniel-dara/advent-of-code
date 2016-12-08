8118eecf7b5458fa90848071e1bad1c6c5ec69fc
Daniel Dara <ddara@g.clemson.edu>
2016-12-08 02:32:13 -0500

    Added day8 solutions.

---
+++ 2016/day8/part2.py
@@ -0,0 +1,32 @@
+import re
+
+NUM_PATTERN   = re.compile(r'\d+')
+ROWS, COLUMNS = 6, 50
+
+pixels = [[0] * COLUMNS for r in range(ROWS)]
+
+def rotateCol(col, rotate_by):
+	newCol = [pixels[(i - rotate_by) % ROWS][col] for i in range(ROWS)]
+
+	for i in range(ROWS):
+		pixels[i][col] = newCol[i]
+
+def rotateRow(row, rotate_by):
+	pixels[row] = [pixels[row][(i - rotate_by) % COLUMNS] for i in range(COLUMNS)]
+
+for line in open('input.txt'):
+	a, b = map(int, NUM_PATTERN.findall(line))
+
+	if line.startswith('rect'):
+		for c in range(a):
+			for r in range(b):
+				pixels[r][c] = 1
+	elif line.startswith('rotate column'):
+		rotateCol(a, b)
+	elif line.startswith('rotate row'):
+		rotateRow(a, b)
+
+for r in range(ROWS):
+	for c in range(COLUMNS):
+		print('#' if pixels[r][c] else ' ', end='')
+	print()

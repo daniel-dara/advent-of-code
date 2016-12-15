d25a29d253611a6c57bb60182fda82c7c2a82420
Daniel Dara <ddara@g.clemson.edu>
2016-12-15 01:24:20 -0500

    Added 2016 day15 solution.

---
+++ 2016/day15/part2.py
@@ -0,0 +1,17 @@
+discs = ((7,  0), (13, 0), (3,  2), (5,  2), (17, 0), (19, 7), (11, 0))
+
+for start_time in range(0, 10000000):
+	time = start_time
+	level = 0
+
+	while level < len(discs):
+		time += 1
+
+		if (discs[level][1] + time) % discs[level][0] != 0:
+			break;
+
+		level += 1
+
+	if level == len(discs):
+		print('start_time:', start_time)
+		exit()

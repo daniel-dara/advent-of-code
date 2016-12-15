266f17e644f3df55dcdcb61705c86d9c84a2115a
Daniel Dara <ddara@g.clemson.edu>
2016-12-15 02:51:29 -0500

    Lots of cleanup, more to come.

--- 2016/day11/part1.py
+++ 2016/day11/part1.py
@@ -1,17 +1,31 @@
 import copy
 
 # {elevator}-[0M,0G,1M],[1G],[],[]
-def getHash(floors, elevator):
+def getHash(elevator, floors):
 	return str(elevator) + str([(len(floors[i]), sum(x[1] == 'G' for x in floors[i])) for i in range(4)])
 
-def move(direction, elevator, item, floors, index = -1):
-	floors[elevator].remove(item)
-	index = index if index != -1 else len(floors[elevator + direction])
-	floors[elevator + direction].insert(index, item)
+def move(elevator, floors, direction, from_index, to_index):
+	floors[elevator + direction].insert(to_index, floors[elevator].pop(from_index))
+
+def exploreState(elevator, floors, direction, moves, cur_hash, index1, index2 = None):
+	if index2:
+		move(elevator, floors, direction, index2, 0)
+
+	move(elevator, floors, direction, index1, 0)
+
+	next_hash = getHash(elevator + direction, floors)
+
+	if next_hash not in states and isValidState(elevator + direction, floors) and isValidState(elevator, floors, True):
+		queue.append([elevator + direction, copy.deepcopy(floors), moves + 1, cur_hash, -direction, next_hash])
+
+	move(elevator + direction, floors, -direction, 0, index1)
+
+	if index2:
+		move(elevator + direction, floors, -direction, 0, index2)
 
 # State is valid if the elevator floor is non-empty and floors with generators do not have any unpaired chips.
-def isValidState(floors, elevator, allow_empty = False):
-	if not floors[elevator] and not allow_empty:
+def isValidState(elevator, floors, allow_empty_floors = False):
+	if not allow_empty_floors and not floors[elevator]:
 		return False
 
 	hasGen = False
@@ -28,94 +42,58 @@ def isValidState(floors, elevator, allow_empty = False):
 def explore(queue):
 	while queue:
 		node = queue.pop(0)
-		elevator, floors, moves, last_state = node
-		val = getHash(floors, elevator)
+		elevator, floors, moves, last_state, last_dir, cur_hash = node
 
 		# Return if we've already reached this state.
-		if val in states:
+		if cur_hash in states:
 			continue
 
 		# Set the move count for the current state.
-		states[val] = {'moves': moves, 'last': last_state}
+		states[cur_hash] = {'moves': moves, 'last': last_state}
 
-		if elevator == 3 and val == getHash(end, 3):
-			print("FOUND SOLUTION", moves)
+		if elevator == 3 and cur_hash == getHash(3, end):
+			print("FOUND SOLUTION:", moves)
 			return
 
 		# Explore new states, move up/down with 1/2 items.
 		for index1 in range(len(floors[elevator])):
-			item = floors[elevator][index1]
 
 			for index2 in range(index1 + 1, len(floors[elevator])):
-				item2 = floors[elevator][index2]
-
-				if item == item2:
-					continue
-
 				if elevator < 3:
-					move(1, elevator, item, floors)
-					move(1, elevator, item2, floors)
-
-					if getHash(floors, elevator + 1) not in states and isValidState(floors, elevator + 1) and isValidState(floors, elevator, True):
-						queue.append([elevator + 1, copy.deepcopy(floors), moves + 1, val])
-
-					move(-1, elevator + 1, item, floors, index1)
-					move(-1, elevator + 1, item2, floors, index2)
+					exploreState(elevator, floors, 1, moves, cur_hash, index1, index2)
 
 				if elevator > 0:
-					move(-1, elevator, item, floors)
-					move(-1, elevator, item2, floors)
-
-					if getHash(floors, elevator - 1) not in states and isValidState(floors, elevator - 1) and isValidState(floors, elevator, True):
-						queue.append([elevator - 1, copy.deepcopy(floors), moves + 1, val])
-
-					move(1, elevator - 1, item, floors, index1)
-					move(1, elevator - 1, item2, floors, index2)
+					exploreState(elevator, floors, -1, moves, cur_hash, index1, index2)
 
 			if elevator < 3:
-				move(1, elevator, item, floors)
-
-				if getHash(floors, elevator + 1) not in states and isValidState(floors, elevator + 1) and isValidState(floors, elevator, True):
-					queue.append([elevator + 1, copy.deepcopy(floors), moves + 1, val])
-
-				move(-1, elevator + 1, item, floors, index1)
+				exploreState(elevator, floors, 1, moves, cur_hash, index1)
 
 			if elevator > 0:
-				move(-1, elevator, item, floors)
-
-				if getHash(floors, elevator - 1) not in states and isValidState(floors, elevator - 1) and isValidState(floors, elevator, True):
-					queue.append([elevator - 1, copy.deepcopy(floors), moves + 1, val])
-
-				move(1, elevator - 1, item, floors, index1)
+				exploreState(elevator, floors, -1, moves, cur_hash, index1)
 
 useSample = False
 
 if not useSample:
 	# floors = [['PG', 'PM'], ['BG', 'CG', 'RG', 'LG'], ['BM', 'CM', 'RM', 'LM'], []]
-	floors = [['PG', 'PM', 'ZG', 'ZM', 'XG', 'XM'], ['BG', 'CG', 'RG', 'LG'], ['BM', 'CM', 'RM', 'LM'], []] # 33 is right answer
-	# floors = [['PG', 'PM'], ['BG', 'CG', 'RG'], ['BM', 'CM', 'RM'], []]
-	# floors = [['PG', 'PM'], ['BG', 'CG'], ['BM', 'CM'], []]
+	floors = [['PG', 'PM', 'ZG', 'ZM', 'XG', 'XM'], ['BG', 'CG', 'RG', 'LG'], ['BM', 'CM', 'RM', 'LM'], []]
 else:
 	floors = [['HM', 'LM'], ['HG'], ['LG'], []] # 11 moves
 
 end = [[], [], [], sum(floors, [])]
 
-elevator = 0;
+elevator = 0
 states = {}
 queue = []
 
-# print(getHash())
-queue.append([elevator, copy.deepcopy(list(floors)), 0, 'origin'])
-explore(queue)
+queue.append([elevator, copy.deepcopy(list(floors)), 0, 'origin', 0, getHash(elevator, floors)])
 
-for state in states:
-	print(state)
+explore(queue)
 
 print('total states:', len(states))
 
 floors = end
 
-if getHash(end, 3) not in states:
+if getHash(3, end) not in states:
 	print('didn\'t reach final state :(')
 else:
-	print('DONE, moves: '+ str(states[getHash(end, 3)]))
+	print('DONE, moves: '+ str(states[getHash(3, end)]))

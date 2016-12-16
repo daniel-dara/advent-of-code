6ca7e00fc68af726835b81c66e60c26fe7467f15
Daniel Dara <ddara@g.clemson.edu>
2016-12-16 00:23:13 -0500

    Added 2016 day16 solutions.

---
+++ 2016/day16/part2.py
@@ -0,0 +1,22 @@
+
+def dragCurve(data):
+	return data + '0' + data[::-1].replace('1', '2').replace('0', '1').replace('2', '0')
+
+def checkSum(data):
+	while len(data) % 2 == 0:
+		checksum = ''
+
+		for i in range(0, len(data), 2):
+			checksum += str(int(data[i] == data[i + 1]))
+
+		data = checksum
+
+	return checksum
+
+data = '11100010111110100'
+size = 35651584
+
+while len(data) < size:
+	data = dragCurve(data)
+
+print(checkSum(data[:size]))

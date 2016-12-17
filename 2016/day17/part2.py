7660b0f1b0e824d61e28d1880bedd0516da93fc3
Daniel Dara <ddara@g.clemson.edu>
2016-12-17 15:29:17 -0500

    Sacrificed extra line for less indentation. Removed unnecessary variable initialization.

--- 2016/day17/part2.py
+++ 2016/day17/part2.py
@@ -4,30 +4,29 @@ def unlock(path):
 	return [not c.isdigit() and c != 'a' for c in md5(bytes('pxxbnzuo' + path, 'ascii')).hexdigest()[:4]]
 
 def findLongestPath(queue):
-	longest_path = ''
-
 	while queue:
 		x, y, path = queue.pop(0)
 
 		if (x, y) == (3, 3):
 			longest_path = path
-		else:
-			if x < 0 or y < 0 or x >= 4 or y >= 4:
-				continue
+			continue
 
-			unlocked = unlock(path)
+		if x < 0 or y < 0 or x >= 4 or y >= 4:
+			continue
 
-			if unlocked[0]:
-				queue.append((x, y - 1, path + 'U'))
+		unlocked = unlock(path)
 
-			if unlocked[1]:
-				queue.append((x, y + 1, path + 'D'))
+		if unlocked[0]:
+			queue.append((x, y - 1, path + 'U'))
 
-			if unlocked[2]:
-				queue.append((x - 1, y, path + 'L'))
+		if unlocked[1]:
+			queue.append((x, y + 1, path + 'D'))
 
-			if unlocked[3]:
-				queue.append((x + 1, y, path + 'R'))
+		if unlocked[2]:
+			queue.append((x - 1, y, path + 'L'))
+
+		if unlocked[3]:
+			queue.append((x + 1, y, path + 'R'))
 
 	return longest_path
 

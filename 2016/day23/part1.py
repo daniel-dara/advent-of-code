9e5cccecdbc347a9194d4167626c19a6d34085af
Daniel Dara <ddara@g.clemson.edu>
2016-12-23 00:46:11 -0500

    Added 2016 day 23 part1.

---
+++ 2016/day23/part1.py
@@ -0,0 +1,49 @@
+regs = {'a': 7, 'b': 0, 'c': 0, 'd': 0}
+
+instructions = []
+
+for line in open('input.txt'):
+	cmd, a, b, *trash = line.rstrip('\n').split(' ') + [None, None]
+	instructions.append([cmd, a, b])
+
+i = 0
+while i < len(instructions):
+	incr = 1
+	cmd, a, b = instructions[i]
+
+	if cmd == 'cpy':
+		if not b.lstrip('-').isdigit():
+			if a.lstrip('-').isdigit():
+				regs[b] = int(a)
+			else:
+				regs[b] = regs[a]
+	elif cmd == 'jnz':
+		a2 = int(a) if a.lstrip('-').isdigit() else regs[a]
+		b2 = int(b) if b.lstrip('-').isdigit() else regs[b]
+
+		if a2:
+			incr = b2
+	elif cmd == 'tgl':
+		index = i + regs[a]
+
+		if index < len(instructions) and index >= 0:
+			cmd2, a2, b2 = instructions[index]
+
+			if cmd2 == 'inc':
+				cmd2 = 'dec'
+			elif cmd2 == 'dec' or cmd2 == 'tgl':
+				cmd2 = 'inc'
+			elif cmd2 == 'jnz':
+				cmd2 = 'cpy'
+			elif cmd2 == 'cpy':
+				cmd2 = 'jnz'
+
+			instructions[index] = [cmd2, a2, b2]
+	elif cmd == 'dec':
+		regs[a] -= 1
+	elif cmd == 'inc':
+		regs[a] += 1
+
+	i += incr
+
+print(regs)

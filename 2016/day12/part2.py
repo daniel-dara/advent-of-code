cb83408d45c503ae62241e71ea8a71fa70d2ab21
Daniel Dara <ddara@g.clemson.edu>
2017-12-09 23:45:04 -0500

    Optimized 2016 day12 some.

--- 2016/day12/part2.py
+++ 2016/day12/part2.py
@@ -1,11 +1,8 @@
-# Same as part1 but with a non-zero initial value for register 'c'.
+# A fun little interpreter for some basic instructions. Some parsing plus a basic state machine.
 
 registers = {'a': 0, 'b': 0, 'c': 1, 'd': 0}
 instructions = open('input.txt').readlines()
 index = 0
-
-def isInt(string):
-	return string.lstrip('-').isdigit()
 
 while index < len(instructions):
 	parts = instructions[index].rstrip('\n').split(' ')
@@ -13,16 +10,15 @@ while index < len(instructions):
 	jump = 1
 
 	if cmd == 'cpy':
-		registers[arg2] = int(arg1) if isInt(arg1) else registers[arg1]
+		registers[arg2] = registers[arg1] if arg1 in registers else int(arg1)
 	elif cmd == 'inc':
 		registers[arg1] += 1
 	elif cmd == 'dec':
 		registers[arg1] -= 1
 	else:
-		if isInt(arg1):
-			if int(arg1) != 0:
-				jump = int(arg2)
-		elif registers[arg1] != 0:
+		val = registers[arg1] if arg1 in registers else int(arg1)
+
+		if val != 0:
 			jump = int(arg2)
 
 	index += jump

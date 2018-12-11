b4a590a82ff4ab2a0517ac0daf1bea23bd572458
Daniel Dara <ddara@g.clemson.edu>
2018-12-10 23:30:06 -0500

    Added 2018 day 10.

---
+++ 2018/day10/part2.py
@@ -0,0 +1,52 @@
+import re
+from collections import defaultdict
+
+# Define a point as having a location and a velocity.
+class Point:
+	def __init__(self, x, y, vx=0, vy=0):
+		self.x = x
+		self.y = y
+		self.vx = vx
+		self.vy = vy
+
+	def __repr__(self):
+		return str([(self.x, self.y), (self.vx, self.vy)])
+
+	# Moves the point based on the given time and the point's velocity and current location.
+	def tick(self, seconds=1):
+		self.x += self.vx * seconds
+		self.y += self.vy * seconds
+
+# Print the board given the boundaries
+def printBoard(minX, minY, maxX, maxY):
+	for y in range(minY, maxY + 1):
+		for x in range(minX, maxX + 1):
+			if any(point.x == x and point.y == y for point in points):
+				print('#', end='')
+			else:
+				print('.', end='')
+
+		print()
+
+points = []
+
+for line in open('input.txt'):
+	x, y, vx, vy = map(int, re.findall(r'(-?\d+)', line))
+	points.append(Point(x, y, vx, vy))
+
+height = None
+ticks = 0
+
+# Loop until a reasonable height is reached (human trial and error)
+while height == None or height > 10:
+	ticks += 1
+
+	for point in points:
+		point.tick()
+
+	upperLeft  = Point(min(points, key=lambda point: point.x).x, min(points, key=lambda point: point.y).y)
+	lowerRight = Point(max(points, key=lambda point: point.x).x, max(points, key=lambda point: point.y).y)
+
+	height = lowerRight.y - upperLeft.y + 1
+
+print(ticks)

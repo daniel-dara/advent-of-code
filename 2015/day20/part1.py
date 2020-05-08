60ce7efa7a137d4f2a3d5dea7935edf180e3943e
Daniel Dara <ddara@g.clemson.edu>
2020-05-07 20:04:57 -0400

    Move Position logic into path.

--- 2015/day20/part1.py
+++ 2015/day20/part1.py
@@ -1,28 +1,30 @@
 from __future__ import annotations
-
-from typing import NamedTuple, Optional, Dict, Tuple
+from typing import NamedTuple, Optional, Dict
 
 
 class Position(NamedTuple):
     row: int
     column: int
 
-    def move(self, delta: Position) -> Tuple[Position, Position]:
-        if diagram[self] == '+':
-            for new_delta in DELTAS:
-                if self.apply(new_delta) in diagram and diagram[self.apply(new_delta)] != '*':
-                    delta = new_delta
-                    break
-
-        diagram[self] = '*'
-
-        new_position = self.apply(delta)
-
-        return new_position, delta
-
     def apply(self, delta: Position) -> Position:
         return Position(self.row + delta.row, self.column + delta.column)
 
+
+def path_generator(position: Position, delta: Position) -> Optional[str]:
+    while position in diagram:
+        if diagram[position].isalpha():
+            yield diagram[position]
+
+        if diagram[position] == '+':
+            for new_delta in DELTAS:
+                if position.apply(new_delta) in diagram and diagram[position.apply(new_delta)] != '*':
+                    delta = new_delta
+
+        diagram[position] = '*'
+        position = position.apply(delta)
+
+
+DELTAS = (Position(-1, 0), Position(1, 0), Position(0, 1), Position(0, -1))
 
 diagram: Dict[Position, str] = {
     Position(row, column): character
@@ -31,15 +33,4 @@ diagram: Dict[Position, str] = {
     if character != ' '
 }
 
-DELTAS = (Position(-1, 0), Position(1, 0), Position(0, 1), Position(0, -1))
-
-
-def path(position: Position, delta: Position) -> Optional[str]:
-    while position in diagram:
-        if diagram[position].isalpha():
-            yield diagram[position]
-
-        position, delta = position.move(delta)
-
-
-print(''.join(path(min(diagram.keys()), Position(1, 0))))
+print(''.join(path_generator(min(diagram.keys()), Position(1, 0))))

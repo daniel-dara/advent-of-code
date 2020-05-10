9c7a886e531d99f8bb8f23d5279a7899c7ed7a06
Daniel Dara <ddara@g.clemson.edu>
2020-05-10 13:24:31 -0400

    Cleanup solution some.

--- 2017/day22/solution.py
+++ 2017/day22/solution.py
@@ -1,25 +1,23 @@
-position = tuple(map(lambda x: len(x) // 2, [open('input.txt').readlines(), open('input.txt').readline().rstrip()]))
+infected = {
+    (row, column)
+    for row, line in enumerate(open('input.txt'))
+    for column, character in enumerate(line.rstrip())
+    if character == '#'
+}
 
-infected = set()
-
-for row, line in enumerate(open('input.txt')):
-    for column, character in enumerate(line.rstrip()):
-        if character == '#':
-            infected.add((row, column))
-
-directions = (-1, 0), (0, 1), (1, 0), (0, -1)
-orientation = directions[0]
-
+directions = (-1, 0), (0, 1), (1, 0), (0, -1)  # Up, Right, Down, Left
+position = len(open('input.txt').readlines()) // 2, len(open('input.txt').readline().rstrip()) // 2
+orientation = directions[0]  # Up
 infected_bursts = 0
 
 for _ in range(10_000):
     if position in infected:
-        orientation = directions[(directions.index(orientation) + 1) % len(directions)]
         infected.remove(position)
+        orientation = directions[(directions.index(orientation) + 1) % len(directions)]  # Turn right.
     else:
         infected_bursts += 1
         infected.add(position)
-        orientation = directions[(directions.index(orientation) - 1) % len(directions)]
+        orientation = directions[(directions.index(orientation) - 1) % len(directions)]  # Turn left.
 
     position = position[0] + orientation[0], position[1] + orientation[1]
 

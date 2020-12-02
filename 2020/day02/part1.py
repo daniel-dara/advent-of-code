1994db5cd3b4d2c40a92bf40f5053d1802c0b523
Daniel Dara <ddara@g.clemson.edu>
2020-12-01 22:41:48 -0700

    Use regex literals.

--- 2020/day02/part1.py
+++ 2020/day02/part1.py
@@ -4,7 +4,7 @@ from collections import Counter
 valid_count = 0
 
 for line in open('input.txt'):
-    minimum, maximum, letter, word = re.search('(\\d+)-(\\d+) (\\w): (\\w+)', line).groups()
+    minimum, maximum, letter, word = re.search(r'(\d+)-(\d+) (\w): (\w+)', line).groups()
 
     if int(minimum) <= Counter(word)[letter] <= int(maximum):
         valid_count += 1

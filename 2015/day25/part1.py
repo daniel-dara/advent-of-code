916dd7a2ea8b4bf61179daab83b97e584ae1238c
Daniel Dara <ddara@salesforce.com>
2020-12-10 17:37:00 -0700

    Optimize solution.

--- 2015/day25/part1.py
+++ 2015/day25/part1.py
@@ -1,18 +1,9 @@
-from itertools import count
 
-previous_value = 20151125
+code = 20151125
 row, col = 1, 1
-for _ in count():
-	row -= 1
-	col += 1
-	if row < 1:
-		row = col
-		col = 1
 
-	current_value = (previous_value * 252533) % 33554393
+while not (row == 2981 and col == 3075):
+	row, col = (row - 1, col + 1) if row > 1 else (col + 1, 1)
+	code = (code * 252533) % 33554393
 
-	if row == 2981 and col == 3075:
-		print(current_value)
-		exit()
-	# print(row, col, current_value)
-	previous_value = current_value
+print(code == 9132360)

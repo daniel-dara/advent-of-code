7906629b0f1637b53a52ed92a171eb8d9d5280ca
Daniel Dara <ddara@g.clemson.edu>
2020-12-12 12:34:13 -0700

    Refactor flipping code.

--- 2017/day21/part1.wip.py
+++ 2017/day21/part1.wip.py
@@ -1,56 +1,39 @@
-
+import numpy
 rules = {a: b for a, b in (line.strip().split(' => ') for line in open('input.txt'))}
 image = ['.#./..#/###']
 
 flipped_rules = {}
 
 for input_, output in rules.items():
-	# flipped_rules = {}
+	for k in range(4):
+		array = [list(row) for row in input_.split('/')]
+		array = numpy.rot90(array, k)
+		str_array = '/'.join(''.join(row) for row in array)
+		# print(str_array.replace('/', '\n'), '\n')
+		flipped_str = '/'.join(''.join(row[::-1]) for row in array)
+		# print(flipped_str.replace('/', '\n'), '\n')
 
-	if input_.count('/') == 1:
-		top, bottom = input_.split('/')
-		flipped_rules[bottom[1] + bottom[0] + '/' + top[1] + top[0]] = output
-		flipped_rules[bottom[0] + top[0] + '/' + bottom[1] + top[1]] = output
-		flipped_rules[top[1] + bottom[1] + '/' + top[0] + bottom[0]] = output
+		flipped_rules[str_array] = output
+		flipped_rules[flipped_str] = output
 
-		if bottom[1] + bottom[0] + '/' + top[1] + top[0] in (set(rules) - {input_}):
-			exit(1)
-		if bottom[0] + top[0] + '/' + bottom[1] + top[1] in (set(rules) - {input_}):
-			exit(1)
-		if top[1] + bottom[1] + '/' + top[0] + bottom[0] in (set(rules) - {input_}):
-			exit(1)
-	else:
-		og_input = input_
+rules = flipped_rules
 
-		for _ in range(4):
-			input_ = [list(row) for row in input_.split('/')]
-			new_pattern = [['.'] * 3 for _ in range(3)]
+# Properly split matrix: https://stackoverflow.com/a/11105569/1313439
+# transform, then put back together
+for i in range(5):
+	print(i)
+	print('\n----\n'.join(pattern.replace('/', '\n') for pattern in image))
 
-			for row in range(3):
-				for col in range(3):
-					new_pattern[col][2 - row] = input_[row][col]
-
-			new_pattern = [''.join(row) for row in new_pattern]
-			flipped_rules['/'.join(new_pattern)] = output
-			if '/'.join(new_pattern) in (set(rules) - {og_input}):
-				exit(1)
-			input_ = '/'.join(new_pattern)
-
-			new_pattern[0], new_pattern[2] = new_pattern[2], new_pattern[0]
-			flipped_rules['/'.join(new_pattern)] = output
-			if '/'.join(new_pattern) in (set(rules) - {og_input}):
-				exit(1)
-
-rules = dict(rules.items() | flipped_rules.items())
-
-for x in range(5):
 	new_image = []
 
-	print(x, image)
+	# print(x)
+	# print('\n\n'.join(pattern.replace('/', '\n') for pattern in image))
+
 	for pattern in image:
 		output = rules[pattern]
 
 		if output.count('/') == 3:
+			# convert 4x4 to 2x2
 			rows = output.split('/')
 			new_image.append(rows[0][:2] + '/' + rows[1][:2])
 			new_image.append(rows[0][2:] + '/' + rows[1][2:])
@@ -61,7 +44,6 @@ for x in range(5):
 
 	image = new_image
 
-print(x + 1, image)
 print(sum(pattern.count('#') for pattern in image))
 
 # 136 too low

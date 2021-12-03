2e59f3c77cb9b58ef207f46f8f489b139414a9cb
Daniel Dara <ddara@g.clemson.edu>
2021-12-03 11:23:06 -0700

    Run on input.txt

--- 2021/day03/part1.py
+++ 2021/day03/part1.py
@@ -1,7 +1,7 @@
 from collections import Counter
 from math import prod
 
-numbers = open('example.txt').read().split()
+numbers = open('input.txt').read().split()
 columns = zip(*numbers)
 hi_lo_digits = [next(zip(*Counter(row).most_common())) for row in columns]
 bit_array = list(zip(*hi_lo_digits))

c77567d0439c2e501cda562fbd675f96462836b6
Daniel Dara <ddara@g.clemson.edu>
2021-12-03 10:31:14 -0700

    Remove alternative code.

--- 2021/day03/part1.py
+++ 2021/day03/part1.py
@@ -6,8 +6,3 @@ columns = zip(*numbers)
 hi_lo_digits = [next(zip(*Counter(row).most_common())) for row in columns]
 bit_array = list(zip(*hi_lo_digits))
 print(prod(int(''.join(bits), 2) for bits in bit_array))
-
-# [7, 5, 8, 7, 5]	max 12 per column
-# low = int(''.join(str(int(digit_sum < len(numbers) / 2)) for digit_sum in sums), 2)
-# high = int(''.join(str(int(digit_sum > len(numbers) / 2)) for digit_sum in sums), 2)
-# print(high * low)

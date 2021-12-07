b94a65f4e1036b95915839bc9ee64a469c3b7887
Daniel Dara <ddara@salesforce.com>
2021-12-06 22:59:56 -0700

    Update part1 to use median instead of simulation.

--- 2021/day07/part1.py
+++ 2021/day07/part1.py
@@ -1,9 +1,4 @@
+from statistics import median
 
 positions = list(map(int, open('input.txt').read().split(',')))
-
-fuel_per_position = []
-
-for i in range(min(positions), max(positions)):
-	fuel_per_position.append(sum(abs(position - i) for position in positions))
-
-print(min(fuel_per_position))
+print(int(sum(abs(p - median(positions)) for p in positions)))

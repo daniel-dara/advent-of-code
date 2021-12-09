0c4f3496bf1038cf18b5b44dd403ecb0cab085e3
Daniel Dara <ddara@salesforce.com>
2021-12-09 09:16:12 -0700

    Update names.

--- 2021/day09/part2.py
+++ 2021/day09/part2.py
@@ -1,14 +1,14 @@
 from math import prod
 
 
-def get_basin(row_: int, col_: int) -> int:
+def get_basin_size(row_: int, col_: int) -> int:
 	if (row_, col_) not in grid or grid[row_, col_] == 9:
 		return 0
 
 	grid[row_, col_] = 9
 
 	return 1 + sum(
-		get_basin(row2, col2)
+		get_basin_size(row2, col2)
 		for row2, col2 in ((row_ - 1, col_), (row_ + 1, col_), (row_, col_ - 1), (row_, col_ + 1))
 	)
 
@@ -19,10 +19,10 @@ grid = {
 	for col, char in enumerate(line.strip())
 }
 
-basins = (
-	get_basin(row, col)
+basin_sizes = (
+	get_basin_size(row, col)
 	for row, col in grid
 	if grid[row, col] != 9
 )
 
-print(prod(sorted(basins)[-3:]))
+print(prod(sorted(basin_sizes)[-3:]))

40505893ff82ddcb5b2623ca2ee64840f67c3e57
Daniel Dara <ddara@g.clemson.edu>
2021-12-08 23:25:54 -0700

    Remove unnecessary brackets.

--- 2020/day10/part2.py
+++ 2020/day10/part2.py
@@ -6,4 +6,4 @@ ratings = sorted(map(int, open('input.txt').readlines()))
 ratings = [0] + ratings + [ratings[-1] + 3]
 differences = (b - a for a, b in zip(ratings, ratings[1:]))
 group_lengths = map(len, re.findall(r'(1{2,})', ''.join(map(str, differences))))
-print(prod([1 + comb(length - 1, 1) + comb(length - 1, 2) for length in group_lengths]))
+print(prod(1 + comb(length - 1, 1) + comb(length - 1, 2) for length in group_lengths))

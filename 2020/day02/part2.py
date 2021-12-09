1e920964c98617459fb2ea3f70a2969a33cc634b
Daniel Dara <ddara@g.clemson.edu>
2021-12-08 23:25:52 -0700

    Use regex literals.

--- 2020/day02/part2.py
+++ 2020/day02/part2.py
@@ -3,7 +3,7 @@ import re
 valid_count = 0
 
 for line in open('input.txt'):
-    position1, position2, letter, word = re.search('(\\d+)-(\\d+) (\\w): (\\w+)', line).groups()
+    position1, position2, letter, word = re.search(r'(\d+)-(\d+) (\w): (\w+)', line).groups()
 
     if (word[int(position1) - 1] == letter) ^ (word[int(position2) - 1] == letter):
         valid_count += 1

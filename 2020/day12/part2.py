99d7f4ac40f6dc8dbdec8cee8e963fb77698b5d0
Daniel Dara <ddara@g.clemson.edu>
2021-12-08 23:25:54 -0700

    Remove old part2 solutions.

--- 2020/day12/part2.py
+++ 2020/day12/part2.py
@@ -1,16 +1,6 @@
-import math
-
 cardinals = {'N': (0, 1), 'E': (1, 0), 'S': (0, -1), 'W': (-1, 0)}
 ship = 0, 0
 waypoint = 10, 1
-
-
-def rotate(point, radians):
-	x, y = point
-	new_x = x * math.cos(radians) + y * math.sin(radians)
-	new_y = -x * math.sin(radians) + y * math.cos(radians)
-	return round(new_x), round(new_y)
-
 
 for line in open('input.txt'):
 	action, value = line[0], int(line.strip()[1:])
@@ -20,6 +10,8 @@ for line in open('input.txt'):
 	elif action == 'F':
 		ship = ship[0] + value * waypoint[0], ship[1] + value * waypoint[1]
 	else:
-		waypoint = rotate(waypoint, (value if action == 'R' else -value) * math.pi / 180)
+		x, y = waypoint
+		clockwise_degrees = {'L': 360 - value, 'R': value}[action]
+		waypoint = {90: (y, -x), 180: (-x, -y), 270: (-y, x)}[clockwise_degrees]
 
-print(sum(map(abs, ship)) == 71586)
+print(sum(map(abs, ship)))

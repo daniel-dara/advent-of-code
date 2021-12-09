9b320714c41031e7679ac76acb9c9783c2aa3287
Daniel Dara <ddara@g.clemson.edu>
2021-12-08 23:25:55 -0700

    Finish converting string solution.

--- 2017/day21/part1.string.py
+++ 2017/day21/part1.string.py
@@ -1,51 +1,60 @@
-# TODO: Finish solving using string matrices.
-
+# Random challenge: solve using strings instead of 2d arrays and numpy.
 import math
-import numpy
 
-rules = {a: b.replace('/', '') for a, b in (line.strip().split(' => ') for line in open('input.txt'))}
+
+def get_chunk_index(index, size, chunk_size):
+	chunks_per_side = size // chunk_size
+	return index // (size * chunk_size) * chunks_per_side + index // chunk_size % chunks_per_side
+
+
+def rotate_clockwise(string, k):
+	if k == 0:
+		return string
+
+	if len(string) == 4:
+		string = ''.join(string[i] for i in [2, 0, 3, 1])
+	else:
+		string = ''.join(string[i] for i in [6, 3, 0, 7, 4, 1, 8, 5, 2])
+
+	return rotate_clockwise(string, k - 1)
+
+
+def flip(string):
+	if len(string) == 4:
+		return ''.join(string[i] for i in [2, 0, 3, 1])
+	else:
+		return ''.join(string[i] for i in [6, 7, 8, 3, 4, 5, 0, 1, 2])
+
+
 image = '.#...####'
-# image = '#..#........#..#'
-# image = '##.##.#..#........##.##.#..#........'
+enhancements = {}
 
-flipped_rules = {}
+for line in open('input.txt'):
+	input_, output = (pattern.replace('/', '') for pattern in line.strip().split(' => '))
 
-for input_, output in rules.items():
 	for k in range(4):
-		array = [list(row) for row in input_.split('/')]
-		array = numpy.rot90(array, k)
-		str_array = ''.join(''.join(row) for row in array)
-		# print(str_array.replace('/', '\n'), '\n')
-		flipped_str = ''.join(''.join(row[::-1]) for row in array)
-		# print(flipped_str.replace('/', '\n'), '\n')
-
-		flipped_rules[str_array] = output
-		flipped_rules[flipped_str] = output
-
-rules = flipped_rules
+		enhancements[rotate_clockwise(input_, k)] = output
+		enhancements[rotate_clockwise(flip(input_), k)] = output
 
 for _ in range(5):
 	size = int(math.sqrt(len(image)))
 	chunk_size = 3 if size % 2 else 2
-	chunks = [''] * (size // chunk_size) ** 2
 	chunks_per_side = size // chunk_size
+	chunks = [''] * chunks_per_side ** 2
+	new_image = ''
 
 	for index, char in enumerate(image):
-		chunk_index = index // (size * chunk_size) * chunks_per_side + index // chunk_size % chunks_per_side
+		chunk_index = get_chunk_index(index, size, chunk_size)
 		chunks[chunk_index] += char
 
-	print(chunks)
+	new_chunks = [enhancements[chunk] for chunk in chunks]
+	size += chunks_per_side
+	chunk_size += 1
 
-	for i in range(len(chunks)):
-		chunks[i] = rules[chunks[i]]
-
-	new_image = ''
-	new_size = size + chunks_per_side
-	new_chunk_size = chunk_size + 1
-	for index in range(new_size ** 2):
-		chunk_index = index // (new_size * new_chunk_size) * chunks_per_side + index // new_chunk_size % chunks_per_side
-		new_image += chunks[chunk_index][(index % (new_chunk_size ** 2 * chunks_per_side)) // new_size * new_chunk_size + 0 + index % new_chunk_size]
-		# new_image += chunks[chunk_index][(index // new_size * new_chunk_size + index % new_chunk_size) % (new_chunk_size ** 2 * chunks_per_side)]
+	for index in range(size ** 2):
+		chunk_index = get_chunk_index(index, size, chunk_size)
+		chunk_column = (index // size) % chunk_size * chunk_size + index % chunk_size
+		new_image += new_chunks[get_chunk_index(index, size, chunk_size)][chunk_column]
 
 	image = new_image
 

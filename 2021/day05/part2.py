3bfe7896115257fe2c6ad0cbc197115a4fb14fbe
Daniel Dara <ddara@g.clemson.edu>
2021-12-08 23:25:57 -0700

    Simplify day 5 further.

--- 2021/day05/part2.py
+++ 2021/day05/part2.py
@@ -1,24 +1,16 @@
 import re
 from collections import defaultdict
-from typing import Iterable
-
-
-def get_range(a1: int, a2: int, b1: int, b2: int) -> Iterable:
-	if a1 == a2:
-		a_range = (abs(b2 - b1) + 1) * [a1]
-	else:
-		a_step = 1 if a2 > a1 else -1
-		a_range = range(a1, a2 + a_step, a_step)
-
-	return a_range
-
 
 graph = defaultdict(int)
 
 for line in open('input.txt'):
 	x1, y1, x2, y2 = map(int, re.findall(r'\d+', line))
 
-	for a, b in zip(get_range(x1, x2, y1, y2), get_range(y1, y2, x1, x2)):
-		graph[(a, b)] += 1
+	graph[x1, y1] += 1
 
-print(sum(value >= 2 for value in graph.values()))
+	while (x1, y1) != (x2, y2):
+		x1 += (x2 > x1) - (x1 > x2)
+		y1 += (y2 > y1) - (y1 > y2)
+		graph[x1, y1] += 1
+
+print(sum(value > 1 for value in graph.values()))

873ba9153554ffbb0c05769f8ba81cf38d4ae9ef
Daniel Dara <ddara@g.clemson.edu>
2021-12-08 23:25:57 -0700

    Cleanup day 5.

--- 2021/day05/part1.py
+++ 2021/day05/part1.py
@@ -1,20 +1,25 @@
 import re
 from collections import defaultdict
+from typing import Iterable
+
+
+def get_range(a1: int, a2: int, b1: int, b2: int) -> Iterable:
+	if a1 == a2:
+		a_range = (abs(b2 - b1) + 1) * [a1]
+	else:
+		a_step = 1 if a2 > a1 else -1
+		a_range = range(a1, a2 + a_step, a_step)
+
+	return a_range
+
 
 graph = defaultdict(int)
 
 for line in open('input.txt'):
 	x1, y1, x2, y2 = map(int, re.findall(r'\d+', line))
 
-	if x1 == x2:
-		y1, y2 = min(y1, y2), max(y1, y2)
+	if x1 == x2 or y1 == y2:
+		for a, b in zip(get_range(x1, x2, y1, y2), get_range(y1, y2, x1, x2)):
+			graph[(a, b)] += 1
 
-		for y in range(y1, y2 + 1):
-			graph[(x1, y)] += 1
-	elif y1 == y2:
-		x1, x2 = min(x1, x2), max(x1, x2)
-
-		for x in range(x1, x2 + 1):
-			graph[(x, y1)] += 1
-
-print(sum(1 for value in graph.values() if value >= 2))
+print(sum(value >= 2 for value in graph.values()))
--- 2021/day05/part2.py
+++ 2021/day05/part2.py
@@ -1,26 +1,24 @@
 import re
 from collections import defaultdict
+from typing import Iterable
+
+
+def get_range(a1: int, a2: int, b1: int, b2: int) -> Iterable:
+	if a1 == a2:
+		a_range = (abs(b2 - b1) + 1) * [a1]
+	else:
+		a_step = 1 if a2 > a1 else -1
+		a_range = range(a1, a2 + a_step, a_step)
+
+	return a_range
+
 
 graph = defaultdict(int)
 
 for line in open('input.txt'):
 	x1, y1, x2, y2 = map(int, re.findall(r'\d+', line))
 
-	if x1 == x2:
-		y1, y2 = min(y1, y2), max(y1, y2)
+	for a, b in zip(get_range(x1, x2, y1, y2), get_range(y1, y2, x1, x2)):
+		graph[(a, b)] += 1
 
-		for y in range(y1, y2 + 1):
-			graph[(x1, y)] += 1
-	elif y1 == y2:
-		x1, x2 = min(x1, x2), max(x1, x2)
-
-		for x in range(x1, x2 + 1):
-			graph[(x, y1)] += 1
-	else:
-		x_step = 1 if x2 > x1 else -1
-		y_step = 1 if y2 > y1 else -1
-
-		for a, b in zip(range(x1, x2 + x_step, x_step), range(y1, y2 + y_step, y_step)):
-			graph[(a, b)] += 1
-
-print(sum(1 for value in graph.values() if value >= 2))
+print(sum(value >= 2 for value in graph.values()))

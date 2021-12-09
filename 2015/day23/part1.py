8b22db8905b951a62f0402e593c95e11ae021b12
Daniel Dara <ddara@g.clemson.edu>
2021-12-08 23:25:53 -0700

    Added 2015 day23.

---
+++ 2015/day23/part1.py
@@ -0,0 +1,23 @@
+program = [(line.strip().replace(',', '') + ' 0').split(' ')[:3] for line in open('input.txt').read().split('\n')]
+registers = {'a': 0, 'b': 0}
+i = 0
+
+while i < len(program):
+	instruction, a, b = program[i]
+
+	if instruction == 'hlf':
+		registers[a] /= 2
+	elif instruction == 'tpl':
+		registers[a] *= 3
+	elif instruction == 'inc':
+		registers[a] += 1
+	elif instruction == 'jmp':
+		i += int(a) - 1
+	elif instruction == 'jie' and registers[a] % 2 == 0:
+		i += int(b) - 1
+	elif instruction == 'jio' and registers[a] == 1:
+		i += int(b) - 1
+
+	i += 1
+
+print(registers['b'])

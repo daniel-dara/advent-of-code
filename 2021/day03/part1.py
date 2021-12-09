59dcc7cd8aaf5409875c45e97547bbc5f1e930cd
Daniel Dara <ddara@g.clemson.edu>
2021-12-08 23:25:56 -0700

    Factor out Counter dependency.

--- 2021/day03/part1.py
+++ 2021/day03/part1.py
@@ -1,8 +1,7 @@
-from collections import Counter
 from math import prod
 
 numbers = open('input.txt').read().split()
 columns = zip(*numbers)
-hi_lo_paired_digits = [next(zip(*Counter(row).most_common())) for row in columns]
+hi_lo_paired_digits = [('1', '0') if row.count('1') > len(row) / 2 else ('0', '1') for row in columns]
 hi_lo_bit_arrays = list(zip(*hi_lo_paired_digits))
 print(prod(int(''.join(bit_array), 2) for bit_array in hi_lo_bit_arrays))

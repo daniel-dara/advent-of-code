8b99478cca6a634c989c3c08af3ab31d29acc8ae
Daniel Dara <ddara@g.clemson.edu>
2021-12-08 23:25:57 -0700

    Add 2021 day 6 part 1.

---
+++ 2021/day06/part1.py
@@ -0,0 +1,15 @@
+timers = list(map(int, open('input.txt').read().split(',')))
+
+for _ in range(256):
+	i = 0
+
+	while i < len(timers):
+		if timers[i] == 0:
+			timers[i] = 6
+			timers.append(9)
+		else:
+			timers[i] -= 1
+
+		i += 1
+
+print(len(timers))

c2335e23832b461d89526c6f7b077cf9d5d43cb5
Daniel Dara <ddara@g.clemson.edu>
2021-12-08 23:25:57 -0700

    Cleanup part 2.

--- 2021/day07/part2.py
+++ 2021/day07/part2.py
@@ -1,9 +1,13 @@
 
-positions = list(map(int, open('input.txt').read().split(',')))
+def gauss_sum(n: int) -> int:
+	return n * (n + 1) // 2
 
-fuel_per_position = []
 
-for i in range(min(positions), max(positions)):
-	fuel_per_position.append(sum(abs(position - i) * (abs(position - i) + 1) // 2 for position in positions))
+positions = sorted(map(int, open('input.txt').read().split(',')))
+
+fuel_per_position = (
+	sum(gauss_sum(abs(p - i)) for p in positions)
+	for i in range(positions[-1])
+)
 
 print(min(fuel_per_position))

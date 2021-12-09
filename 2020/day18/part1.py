95cbe577462f3b6ed71897f36d4d2084fe15e47c
Daniel Dara <ddara@g.clemson.edu>
2021-12-08 23:25:56 -0700

    Cleanup part1.

--- 2020/day18/part1.py
+++ 2020/day18/part1.py
@@ -9,25 +9,18 @@ for equation in equations:
 	value_stack = []
 
 	for part in equation:
-		if part == ')':
-			op_stack.pop()
-			while len(value_stack) >= 2 and op_stack[-1] != '(':
-				a, b, op = value_stack.pop(), value_stack.pop(), op_map[op_stack.pop()]
-				value_stack.append(op(a, b))
-		elif part in op_map:
+		if part in op_map:
 			op_stack.append(part)
 		else:
-			value_stack.append(int(part))
+			if part == ')':
+				op_stack.pop()
+			else:
+				value_stack.append(int(part))
 
 			while len(value_stack) >= 2 and op_stack[-1] != '(':
 				a, b, op = value_stack.pop(), value_stack.pop(), op_map[op_stack.pop()]
-				value_stack.append(op(a, b))
+				value_stack.append(op(b, a))
 
-		print(op_stack, value_stack)
 	values.append(value_stack[0])
-
-	# while len(value_stack) >= 2:
-	# 	a, b, op = value_stack.pop(), value_stack.pop(), op_map[op_stack.pop()]
-	# 	value_stack.append(op(a, b))
 
 print(sum(values))

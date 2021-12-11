6ad3434d0a4de53b3d2f05356fe8bd6c69ea62ff
Daniel Dara <ddara@g.clemson.edu>
2021-12-11 09:17:22 -0700

    Simplify summing.

--- 2021/day11/part1.py
+++ 2021/day11/part1.py
@@ -2,18 +2,18 @@ from itertools import product
 from typing import Set, Tuple
 
 
-def increment(row: int, col: int, flashed: Set[Tuple[int, int]]) -> int:
-	if (row, col) not in grid or (row, col) in flashed:
+def increment(row: int, col: int, flashed_: Set[Tuple[int, int]]) -> int:
+	if (row, col) not in grid or (row, col) in flashed_:
 		return 0
 
 	grid[row, col] += 1
 
 	if grid[row, col] > 9:
 		grid[row, col] = 0
-		flashed.add((row, col))
+		flashed_.add((row, col))
 
 		return 1 + sum(
-			increment(row - row_delta, col - col_delta, flashed)
+			increment(row - row_delta, col - col_delta, flashed_)
 			for row_delta, col_delta in set(product((-1, 0, 1), repeat=2)) - {0, 0}
 		)
 
@@ -29,8 +29,6 @@ total = 0
 
 for _ in range(100):
 	flashed = set()
-
-	for row, col in grid:
-		total += increment(row, col, flashed)
+	total += sum(increment(row, col, flashed) for row, col in grid)
 
 print(total)

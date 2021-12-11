daa78cf83ec3ed97db4323bf58e50bbdf29dd515
Daniel Dara <ddara@g.clemson.edu>
2021-12-10 22:34:23 -0700

    Cleanup part 1.

--- 2021/day11/part1.py
+++ 2021/day11/part1.py
@@ -1,40 +1,36 @@
+from itertools import product
+from typing import Set, Tuple
 
-total = 0
-flashed = set()
+
+def increment(row: int, col: int, flashed: Set[Tuple[int, int]]) -> int:
+	if (row, col) in flashed or row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]):
+		return 0
+
+	grid[row][col] += 1
+
+	if grid[row][col] > 9:
+		grid[row][col] = 0
+		flashed.add((row, col))
+
+		return 1 + sum(
+			increment(row - row_delta, col - col_delta, flashed)
+			for row_delta, col_delta in set(product((-1, 0, 1), repeat=2)) - {0, 0}
+		)
+
+	return 0
+
 
 grid = [
 	list(map(int, line.strip()))
 	for line in open('input.txt')
 ]
-
-
-def incr(row, col):
-	global total, flashed
-
-	if (row, col) in flashed or row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]):
-		return
-
-	grid[row][col] += 1
-
-	if grid[row][col] > 9:
-		total += 1
-		grid[row][col] = 0
-		flashed.add((row, col))
-		incr(row - 1, col)
-		incr(row + 1, col)
-		incr(row, col - 1)
-		incr(row, col + 1)
-		incr(row - 1, col - 1)
-		incr(row - 1, col + 1)
-		incr(row + 1, col + 1)
-		incr(row + 1, col - 1)
-
+total = 0
 
 for _ in range(100):
 	flashed = set()
 
 	for row in range(len(grid)):
 		for col in range(len(grid[0])):
-			incr(row, col)
+			total += increment(row, col, flashed)
 
 print(total)

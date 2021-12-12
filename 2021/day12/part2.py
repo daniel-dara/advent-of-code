139f6cd96647477b82a4654df57eedaced72cfdd
Daniel Dara <ddara@g.clemson.edu>
2021-12-11 23:39:44 -0700

    Cleanup part 2.

--- 2021/day12/part2.py
+++ 2021/day12/part2.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
+from time import time
 
+start = time()
 graph = defaultdict(list)
 
 for line in open('input.txt'):
@@ -7,29 +9,22 @@ for line in open('input.txt'):
 	graph[a].append(b)
 	graph[b].append(a)
 
-path = 'start'
-queue = [path]
+queue = [['start']]
 finished = 0
-visited = set()
 
 while queue:
 	path = queue.pop(0)
-
-	if path in visited:
-		continue
-
-	visited.add(path)
-	last_node = path.split('-')[-1]
-
+	last_node = path[-1]
 
 	for next_node in graph[last_node]:
-		if next_node == 'start' or path[0] == '*' and next_node.islower() and next_node in path:
+		is_repeat = next_node.islower() and next_node in path
+
+		if next_node == 'start' or path[0] == '*' and is_repeat:
 			continue
 		if next_node == 'end':
-			# print(path.strip('*') + '-end')
 			finished += 1
 			continue
 
-		queue.append(('*' if next_node.islower() and next_node in path else '') + path + '-' + next_node)
+		queue.append((['*'] if is_repeat else []) + path + [next_node])
 
 print(finished)

ce28d4cec02c26dda617c408fc19268d0f24c484
Daniel Dara <ddara@g.clemson.edu>
2021-12-12 23:21:11 -0700

    Finish cleaning up part 2.

--- 2021/day13/part2.py
+++ 2021/day13/part2.py
@@ -1,33 +1,29 @@
 
 dots = set()
-folds = []
 
 for line in open('input.txt'):
 	if ',' in line:
 		dots.add(tuple(map(int, line.split(','))))
 	elif '=' in line:
-		folds.append(('y' in line, int(line.split('=')[1])))
+		axis = 'y' in line
+		value = int(line.split('=')[1])
+		folded_dots = set()
 
-for axis, value in folds:
-	folded_dots = set()
-	# axis = axis == 'y'
+		for dot in dots:
+			if dot[axis] < value:
+				folded_dots.add(dot)
+			else:
+				a = dot[not axis], 2 * value - dot[axis]
+				folded_dots.add(a if axis else a[::-1])
 
-	for dot in dots:
-		if dot[axis] < value:
-			folded_dots.add(dot)
-		else:
-			a = dot[not axis], 2 * value - dot[axis]
-			folded_dots.add(a if axis else a[::-1])
+		dots = folded_dots
 
-	dots = folded_dots
-
-output = '\n'.join(
-	''.join(
-		'#' if (x, y) in dots else '.'
-		for x in range(max(x for x, _ in dots) + 1)
+print(
+	'\n'.join(
+		''.join(
+			'#' if (x, y) in dots else '.'
+			for x in range(max(x for x, _ in dots) + 1)
+		)
+		for y in range(max(y for _, y in dots) + 1)
 	)
-	for y in range(max(y for _, y in dots) + 1)
 )
-
-print(output == '.##....##..##..#..#.###...##..###..###.\n#..#....#.#..#.#.#..#..#.#..#.#..#.#..#\n#.......#.#....##...###..#..#.#..#.###.\n#.......#.#....#.#..#..#.####.###..#..#\n#..#.#..#.#..#.#.#..#..#.#..#.#....#..#\n.##...##...##..#..#.###..#..#.#....###.')
-print(output)

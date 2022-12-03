5ad1839d529b658ce91496ed372a36564df6ff48
Daniel Dara <ddara@g.clemson.edu>
2022-12-02 22:48:24 -0700

    Golf harder.

--- 2022/day03/part2.py
+++ 2022/day03/part2.py
@@ -1,13 +1,9 @@
-
 lines = open('input.txt').readlines()
 
 print(
 	sum(
 		(
-			a := lines[i].strip(),
-			b := lines[i + 1],
-			c := lines[i + 2],
-			letter := (set(a) & set(b) & set(c)).pop(),
+			letter := (set.intersection(*map(set, lines[i:i + 3])) - set('\n')).pop(),
 			ord(letter) - (ord('a') - 1 if letter.islower() else ord('A') - 27)
 		)[-1]
 		for i in range(0, len(lines), 3)

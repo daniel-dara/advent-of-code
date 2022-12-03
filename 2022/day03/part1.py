c574c7d591ec6318508a8c6dcc7f4858146e8639
Daniel Dara <ddara@g.clemson.edu>
2022-12-02 23:19:03 -0700

    Improve solution with ascii_letters.

--- 2022/day03/part1.py
+++ 2022/day03/part1.py
@@ -1,9 +1,10 @@
+import string
 print(
 	sum(
 		(
 			mid := len(line) // 2,
 			letter := (set(line[:mid]) & (set(line[mid:]))).pop(),
-			ord(letter) - (ord('a') - 1 if letter.islower() else ord('A') - 27)
+			string.ascii_letters.index(letter) + 1
 		)[-1]
 		for line in open('input.txt')
 	)
--- 2022/day03/part2.py
+++ 2022/day03/part2.py
@@ -1,11 +1,14 @@
+import string
+
 lines = open('input.txt').readlines()
 
 print(
 	sum(
 		(
-			letter := (set.intersection(*map(set, lines[i:i + 3])) - set('\n')).pop(),
-			ord(letter) - (ord('a') - 1 if letter.islower() else ord('A') - 27)
+			letter := (set.intersection(*map(set, lines[i:i + 3])) - {'\n'}).pop(),
+			string.ascii_letters.index(letter) + 1
 		)[-1]
 		for i in range(0, len(lines), 3)
 	)
 )
+

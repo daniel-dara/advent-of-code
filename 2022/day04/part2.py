40aabb3e59ded0073c595be0f566991df6b2bac4
Daniel Dara <ddara@g.clemson.edu>
2022-12-04 07:53:36 -0700

    Simplify part2 condition.

--- 2022/day04/part2.py
+++ 2022/day04/part2.py
@@ -5,7 +5,7 @@ total = 0
 for line in open('input.txt'):
 	a, b, c, d = map(int, re.findall(r'\d+', line))
 
-	if c <= a <= d or c <= b <= d or a <= c <= b or a <= d <= b:
+	if a <= d and b >= c:
 		total += 1
 
 print(total)

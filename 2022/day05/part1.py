074dc54f8b3ae902b520dbec56b2f7583d995e74
Daniel Dara <ddara@g.clemson.edu>
2022-12-04 22:15:36 -0700

    Add 2022 day05 sloppy solution.

---
+++ 2022/day05/part1.py
@@ -0,0 +1,26 @@
+import re
+import itertools
+import collections
+import math
+
+# input_file = 'example.txt'
+input_file = 'input.txt'
+
+v = 0
+l = []
+
+STACK_COUNT = 9
+stacks = [[] for _ in range(STACK_COUNT)]
+
+for line in open(input_file):
+	if line.strip().startswith('['):
+		for i in range(STACK_COUNT):
+			if 1 + i * 4 < len(line) and line[1 + i * 4] != ' ':
+				stacks[i].append(line[1 + i * 4])
+	elif line.startswith('move'):
+		a, b, c = map(int, re.findall(r'\d+', line))
+
+		for _ in range(a):
+			stacks[c - 1].insert(0, stacks[b - 1].pop(0))
+
+print(''.join(stacks[i][0] for i in range(STACK_COUNT)))

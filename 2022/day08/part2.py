34afa357c7c4b4d22ec8f51b81d0b12a12cea030
Daniel Dara <ddara@salesforce.com>
2022-12-08 15:46:08 -0800

    Add part2 sloppy.

--- 2022/day08/part2.py
+++ 2022/day08/part2.py
@@ -1,15 +1,15 @@
-import re
-import itertools
-import collections
-import math
+score = 0
+trees = [list(map(int, line.strip())) for line in open('input.txt').readlines()]
+columns = list(map(list, zip(*trees)))
 
-# input_file = 'example.txt'
-input_file = 'input.txt'
+for a in range(len(trees)):
+	for b in range(len(trees[0])):
+		score = max(
+			score,
+			min(b, next(i + 1 for i, v in enumerate(trees[a][:b][::-1] + [9]) if v >= trees[a][b]))
+			* min(len(trees[0]) - b - 1, next(i + 1 for i, v in enumerate(trees[a][b + 1:] + [9]) if v >= trees[a][b]))
+			* min(a, next(i + 1 for i, v in enumerate(columns[b][:a][::-1] + [9]) if v >= columns[b][a]))
+			* min(len(columns[0]) - a - 1, next(i + 1 for i, v in enumerate(columns[b][a + 1:] + [9]) if v >= columns[b][a]))
+		)
 
-v = 0
-l = []
-
-for line in open(input_file):
-	re.findall(r'\d+', line)
-
-print(v)
+print(score)

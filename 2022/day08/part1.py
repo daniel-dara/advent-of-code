937a99cdbc55b7d03d99eb66fe884a0dff4b8e94
Daniel Dara <ddara@salesforce.com>
2022-12-08 12:09:32 -0800

    Cleanup part1.

--- 2022/day08/part1.py
+++ 2022/day08/part1.py
@@ -1,34 +1,18 @@
-import re
-import itertools
-import collections
-import math
-
-# input_file = 'example.txt'
-input_file = 'input.txt'
-
-total = 0
 visible = set()
-trees = [list(map(int, line.strip())) for line in open(input_file).readlines()]
+trees = [list(map(int, line.strip())) for line in open('input.txt').readlines()]
 
 for a in range(len(trees)):
 	maxes = [-1] * 4
 
 	for b in range(len(trees[0])):
-		if trees[a][b] > maxes[0]:
-			visible.add((a, b))
-
-		if trees[a][len(trees[a]) - b - 1] > maxes[1]:
-			visible.add((a, len(trees[a]) - b - 1))
-
-		if trees[b][a] > maxes[2]:
-			visible.add((b, a))
-
-		if trees[len(trees) - b - 1][a] > maxes[3]:
-			visible.add((len(trees) - b - 1, a))
-
-		maxes[0] = max(maxes[0], trees[a][b])
-		maxes[1] = max(maxes[1], trees[a][len(trees[a]) - b - 1])
-		maxes[2] = max(maxes[2], trees[b][a])
-		maxes[3] = max(maxes[3], trees[len(trees) - b - 1][a])
+		for i, (A, B) in enumerate((
+			(a, b),
+			(a, len(trees[a]) - b - 1),
+			(b, a),
+			(len(trees) - b - 1, a)
+		)):
+			if trees[A][B] > maxes[i]:
+				visible.add((A, B))
+				maxes[i] = trees[A][B]
 
 print(len(visible))

dc5b4da5c346dbc2b5b96fefde449e9353bb8bb6
Daniel Dara <ddara@salesforce.com>
2022-12-08 15:55:18 -0800

    Cleanup.

--- 2022/day08/part2.py
+++ 2022/day08/part2.py
@@ -1,15 +1,14 @@
 score = 0
-trees = [list(map(int, line.strip())) for line in open('input.txt').readlines()]
-columns = list(map(list, zip(*trees)))
+rows = [list(map(int, line.strip())) for line in open('input.txt').readlines()]
+columns = list(map(list, zip(*rows)))
 
-for a in range(len(trees)):
-	for b in range(len(trees[0])):
-		score = max(
-			score,
-			min(b, next(i + 1 for i, v in enumerate(trees[a][:b][::-1] + [9]) if v >= trees[a][b]))
-			* min(len(trees[0]) - b - 1, next(i + 1 for i, v in enumerate(trees[a][b + 1:] + [9]) if v >= trees[a][b]))
-			* min(a, next(i + 1 for i, v in enumerate(columns[b][:a][::-1] + [9]) if v >= columns[b][a]))
-			* min(len(columns[0]) - a - 1, next(i + 1 for i, v in enumerate(columns[b][a + 1:] + [9]) if v >= columns[b][a]))
-		)
-
-print(score)
+print(
+	max(
+		min(b, next(i + 1 for i, v in enumerate(rows[a][:b][::-1] + [9]) if v >= rows[a][b]))
+		* min(len(rows[0]) - b - 1, next(i + 1 for i, v in enumerate(rows[a][b + 1:] + [9]) if v >= rows[a][b]))
+		* min(a, next(i + 1 for i, v in enumerate(columns[b][:a][::-1] + [9]) if v >= columns[b][a]))
+		* min(len(columns[0]) - a - 1, next(i + 1 for i, v in enumerate(columns[b][a + 1:] + [9]) if v >= columns[b][a]))
+		for b in range(len(columns))
+		for a in range(len(rows))
+	)
+)

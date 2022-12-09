1bde0c985b4edc8d561ca7f304ccf5b362bb90ca
Daniel Dara <ddara@salesforce.com>
2022-12-08 23:31:59 -0800

    Cleanup

--- 2022/day08/part2.py
+++ 2022/day08/part2.py
@@ -1,13 +1,30 @@
+import math
+
 score = 0
 rows = [list(map(int, line.strip())) for line in open('input.txt').readlines()]
 columns = list(map(list, zip(*rows)))
 
+
+def get_view_length(slice):
+	total = 0
+
+	for tree in slice[1:]:
+		total += 1
+
+		if tree >= slice[0]:
+			break
+
+	return total
+
+
 print(
 	max(
-		min(b, next(i + 1 for i, v in enumerate(rows[a][:b][::-1] + [9]) if v >= rows[a][b]))
-		* min(len(rows[0]) - b - 1, next(i + 1 for i, v in enumerate(rows[a][b + 1:] + [9]) if v >= rows[a][b]))
-		* min(a, next(i + 1 for i, v in enumerate(columns[b][:a][::-1] + [9]) if v >= columns[b][a]))
-		* min(len(columns[0]) - a - 1, next(i + 1 for i, v in enumerate(columns[b][a + 1:] + [9]) if v >= columns[b][a]))
+		math.prod((
+			get_view_length(rows[a][:b + 1][::-1]),
+			get_view_length(rows[a][b:]),
+			get_view_length(columns[b][:a + 1][::-1]),
+			get_view_length(columns[b][a:]),
+		))
 		for b in range(len(columns))
 		for a in range(len(rows))
 	)

09e7c146be6525cf02d227688c928247566599d7
Daniel Dara <ddara@salesforce.com>
2022-12-08 22:44:26 -0800

    Cleanup.

--- 2022/day09/part1.py
+++ 2022/day09/part1.py
@@ -2,19 +2,13 @@ h = t = (0, 0)
 visited = {t}
 
 for line in open('input.txt'):
+	d = line.split()[0]
+
 	for _ in range(int(line.split()[1])):
-		match line.split()[0]:
-			case 'R':
-				h = h[0] + 1, h[1]
-			case 'L':
-				h = h[0] - 1, h[1]
-			case 'U':
-				h = h[0], h[1] - 1
-			case 'D':
-				h = h[0], h[1] + 1
+		h = h[0] + (d == 'R') - (d == 'L'), h[1] + (d == 'D') - (d == 'U')
 
 		if max(abs(t[0] - h[0]), abs(t[1] - h[1])) == 2:
-			t = t[0] + int(t[0] < h[0]) - int(t[0] > h[0]), t[1] + int(t[1] < h[1]) - int(t[1] > h[1])
+			t = t[0] + (t[0] < h[0]) - (t[0] > h[0]), t[1] + (t[1] < h[1]) - (t[1] > h[1])
 
 		visited.add(t)
 
--- 2022/day09/part2.py
+++ 2022/day09/part2.py
@@ -2,27 +2,17 @@ snake = [(0, 0)] * 10
 visited = {snake[-1]}
 
 for line in open('input.txt'):
+	d = line.split()[0]
+
 	for _ in range(int(line.split()[1])):
-		h = snake[0]
-
-		match line.split()[0]:
-			case 'R':
-				h = h[0] + 1, h[1]
-			case 'L':
-				h = h[0] - 1, h[1]
-			case 'U':
-				h = h[0], h[1] - 1
-			case 'D':
-				h = h[0], h[1] + 1
-
-		snake[0] = h
+		snake[0] = snake[0][0] + (d == 'R') - (d == 'L'), snake[0][1] + (d == 'D') - (d == 'U')
 
 		for i in range(1, 10):
 			h = snake[i - 1]
 			t = snake[i]
 
 			if max(abs(t[0] - h[0]), abs(t[1] - h[1])) == 2:
-				t = t[0] + int(t[0] < h[0]) - int(t[0] > h[0]), t[1] + int(t[1] < h[1]) - int(t[1] > h[1])
+				t = t[0] + (t[0] < h[0]) - (t[0] > h[0]), t[1] + (t[1] < h[1]) - (t[1] > h[1])
 
 			snake[i - 1] = h
 			snake[i] = t

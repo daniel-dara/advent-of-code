634574e1e7e94208ffb08df2fc4f9c64eba652d3
Daniel Dara <ddara@g.clemson.edu>
2022-12-11 14:20:07 -0700

    Cleanup.

--- 2022/day11/part1.py
+++ 2022/day11/part1.py
@@ -15,23 +15,23 @@ class Monkey:
 		return self._test(item)
 
 
-monkeys = {}
+monkeys = []
 
 for i, monkey_str in enumerate(open('input.txt').read().split('\n\n')):
 	lines = monkey_str.split('\n')
 
-	monkeys[i] = Monkey(
+	monkeys.append(Monkey(
 		list(map(int, re.findall(r'\d+', lines[1]))),
 		lambda old, formula=lines[2].split('=')[1]: eval(formula.replace('old', str(old))),
 		lambda worry, x=[int(line.split()[-1]) for line in lines[3:6]]: x[2] if worry % x[0] else x[1],
-	)
+	))
 
 for _ in range(20):
-	for m in monkeys.values():
+	for m in monkeys:
 		while m.items:
 			new_worry = m.operation(m.items.pop(0)) // 3
 			new_monkey = m.test(new_worry)
 			monkeys[new_monkey].items.append(new_worry)
 
-print(prod(sorted(m.test_count for m in monkeys.values())[-2:]))
+print(prod(sorted(m.test_count for m in monkeys)[-2:]))
 

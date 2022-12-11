3993fb6a2e44612c7bfb75fff55d7dcc85e16fd3
Daniel Dara <ddara@g.clemson.edu>
2022-12-11 14:14:03 -0700

    Golf harder.

--- 2022/day11/part1.golf.py
+++ 2022/day11/part1.golf.py
@@ -1,11 +1,17 @@
 M={}
-for i,S in enumerate(open('input.txt').read().split('\n\n')):
+for i,S in enumerate(open(0).read().split('\n\n')):
 	L=S.split('\n')
-	M[i]=[[*map(int,L[1].split(':')[1].split(','))],lambda o,f=L[2].split('=')[1]:eval(f.replace('old',str(o))),lambda w,x=[int(l.split()[-1])for l in L[3:6]]:x[2]if w%x[0]else x[1],0]
+	M[i]=[
+		[*map(int,L[1].split(':')[1].split(','))],
+		lambda o,f=L[2].split('=')[1]:eval(f.replace('old',str(o))),
+		lambda w,x=[int(l.split()[-1])for l in L[3:6]]:x[2]if w%x[0]else x[1],
+		0
+	]
 for _ in range(20):
 	for m in M.values():
 		while m[0]:
 			M[m[2](w:=m[1](m[0].pop(0))//3)][0]+=[w]
 			m[3]+=1
-t=sorted(M[k][3]for k in M)[-2:]
-print(t[-1]*t[-2] == 55930)
+*_,t,u=sorted(M[k][3]for k in M)
+*_,t,u=sorted(M[k][3]for k in M)[-2:]
+print(t*u)

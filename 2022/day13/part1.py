e9b965efd25490f4543f91be2b3d484a3ee604d4
Daniel Dara <ddara@g.clemson.edu>
2022-12-13 20:30:05 -0700

    Add 2022 day13.

---
+++ 2022/day13/part1.py
@@ -0,0 +1,24 @@
+
+def is_ordered(A, B):
+	for a, b in zip(A, B):
+		if type(a) is int and type(b) is int:
+			if a != b:
+				return b - a
+		else:
+			x = is_ordered([a] if type(a) is int else a, [b] if type(b) is int else b)
+
+			if x < 0:
+				return -1
+			elif x > 0:
+				return 1
+
+	return len(B) - len(A)
+
+
+total = 0
+
+for i, pair in enumerate(open('input.txt').read().split('\n\n')):
+	a, b = map(eval, pair.split('\n'))
+	total += (i + 1) * (is_ordered(a, b) >= 0)
+
+print(total == 5185)
---
+++ 2022/day13/part2.py
@@ -0,0 +1,26 @@
+from functools import cmp_to_key
+
+
+def is_ordered(A, B):
+	for a, b in zip(A, B):
+		if type(a) is int and type(b) is int:
+			if a != b:
+				return b - a
+		else:
+			x = is_ordered([a] if type(a) is int else a, [b] if type(b) is int else b)
+
+			if x < 0:
+				return -1
+			elif x > 0:
+				return 1
+
+	return len(B) - len(A)
+
+
+packets = list(map(eval, open('input.txt').read().split())) + [[[2]]] + [[[6]]]
+
+packets = sorted(packets, key=cmp_to_key(is_ordered), reverse=True)
+
+d1 = packets.index([[2]]) + 1
+d2 = packets.index([[6]]) + 1
+print(d1 * d2 == 23751)
